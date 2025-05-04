49ae27ec4fd911206fc30116175d4deef91b7dd6
Alberto Vilar <albertovilar1@gmail.com>
2025-05-04 14:38:58 -0300

    Adjusted custumise validation.

--- src/main/java/com/devmaster/goatfarm/config/exceptions/custom/ValidationError.java
+++ src/main/java/com/devmaster/goatfarm/config/exceptions/custom/ValidationError.java
@@ -19,6 +19,8 @@ public class ValidationError extends CustomError {
     }
 
     public void addError(String fieldName, String message) {
+
+        errors.removeIf(er -> er.getFieldName().equals(fieldName));
         errors.add(new FieldMessage(fieldName, message));
     }
 }
