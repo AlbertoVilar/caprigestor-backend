eb30e35e112c8315b0fd5e9202f4a67f6f41fe5d
Alberto Vilar <albertovilar1@gmail.com>
2025-10-02 15:12:43 -0300

    config: ajusta perfis dev/test/prod e seed inicial no import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,14 +12,14 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2); -- Alberto como OPERA
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2); -- Carlos como OPERATOR
 
 -- Endereços
-INSERT INTO endereco (rua, bairro, cidade, estado, zip_code, pais) VALUES ('Sítio São Felix', 'Zona Rural', 'Santo Andre', 'Paraíba', '58670-000', 'Brasil');
+INSERT INTO endereco (rua, bairro, cidade, estado, cep, pais) VALUES ('Sítio São Felix', 'Zona Rural', 'Santo Andre', 'Paraíba', '58670-000', 'Brasil');
 
-INSERT INTO endereco (rua, bairro, cidade, estado, zip_code, pais) VALUES ('Sítio Jacaré', 'Zona Rural', 'Juazeirinho', 'Paraíba', '58660-000', 'Brasil');
+INSERT INTO endereco (rua, bairro, cidade, estado, cep, pais) VALUES ('Sítio Jacaré', 'Zona Rural', 'Juazeirinho', 'Paraíba', '58660-000', 'Brasil');
 
 -- Fazendas
-INSERT INTO capril (nome, TOD, user_id, address_id, criado, atualizado) VALUES ('Capril Vilar', '16432', 1, 1, NOW(), NOW());
+INSERT INTO capril (nome, TOD, user_id, address_id, criado_em, atualizado_em) VALUES ('Capril Vilar', '16432', 1, 1, NOW(), NOW());
 
-INSERT INTO capril (nome, TOD, user_id, address_id, criado, atualizado) VALUES ('Capril Medeiros', '99887', 2, 2, NOW(), NOW());
+INSERT INTO capril (nome, TOD, user_id, address_id, criado_em, atualizado_em) VALUES ('Capril Medeiros', '99887', 2, 2, NOW(), NOW());
 
 -- Telefones
 INSERT INTO telefone (ddd, numero, goat_farm_id) VALUES ('21', '98988-2934', 1);
@@ -27,36 +27,36 @@ INSERT INTO telefone (ddd, numero, goat_farm_id) VALUES ('83', '99876-1234', 2);
 INSERT INTO telefone (ddd, numero, goat_farm_id) VALUES ('11', '99876-2548', 2);
 
 -- Inserir dados na tabela 'cabras' (BISAVÔS MATERNOS)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1403110395', 'NATAL DO JACOMÉ', 'MALE', 'SAANEN', 'Indefinida', '2010-01-01', 'ATIVO', 1, 1, NULL, NULL);
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1650112018', '12018 CAPRIMEL', 'FEMALE', 'SAANEN', 'Indefinida', '2012-01-01', 'ATIVO', 1, 1, NULL, NULL);
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('2104406006', 'HERE DO ANGICANO', 'MALE', 'SAANEN', 'Indefinida', '2006-01-01', 'ATIVO', 1, 1, NULL, NULL);
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('2114510040', 'TOPÁZIO DO CRS', 'FEMALE', 'SAANEN', 'Indefinida', '2010-01-01', 'ATIVO', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1403110395', 'NATAL DO JACOMÉ', 'MALE', 'SAANEN', 'Indefinida', '2010-01-01', 'ACTIVE', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1650112018', '12018 CAPRIMEL', 'FEMALE', 'SAANEN', 'Indefinida', '2012-01-01', 'ACTIVE', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('2104406006', 'HERE DO ANGICANO', 'MALE', 'SAANEN', 'Indefinida', '2006-01-01', 'ACTIVE', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('2114510040', 'TOPÁZIO DO CRS', 'FEMALE', 'SAANEN', 'Indefinida', '2010-01-01', 'ACTIVE', 1, 1, NULL, NULL);
 
 -- Inserir dados na tabela 'cabras' (BISAVÔS PATERNOS - LADO DO AVÔ)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1422911451', 'BALU DA CAPRIVAMA', 'MALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ATIVO', 1, 1, NULL, NULL);
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1422911408', 'COROA DA CAPRIVAMA', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ATIVO', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1422911451', 'BALU DA CAPRIVAMA', 'MALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ACTIVE', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1422911408', 'COROA DA CAPRIVAMA', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ACTIVE', 1, 1, NULL, NULL);
 
 -- Inserir dados na tabela 'cabras' (BISAVÔS PATERNOS - LADO DA AVÓ)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1412811133', 'SHERIFF SAVANA', 'MALE', 'SAANEN', 'Indefinida', '2011-01-01', 'ATIVO', 1, 1, NULL, NULL);
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1418513119', 'JUCELISE DO JALILI', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ATIVO', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1412811133', 'SHERIFF SAVANA', 'MALE', 'SAANEN', 'Indefinida', '2011-01-01', 'ACTIVE', 1, 1, NULL, NULL);
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1418513119', 'JUCELISE DO JALILI', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ACTIVE', 1, 1, NULL, NULL);
 
 -- Inserir dados na tabela 'cabras' (AVÓS PATERNOS)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1422915618', 'PETRÓLEO CAPRIVAMAR', 'MALE', 'SAANEN', 'Indefinida', '2015-01-01', 'ATIVO', 1, 1, '1422911451', '1422911408');
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1422913470', 'BÉLGICA DA CAPRIVAMAR', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ATIVO', 1, 1, '1412811133', '1418513119');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1422915618', 'PETRÓLEO CAPRIVAMAR', 'MALE', 'SAANEN', 'Indefinida', '2015-01-01', 'ACTIVE', 1, 1, '1422911451', '1422911408');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1422913470', 'BÉLGICA DA CAPRIVAMAR', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ACTIVE', 1, 1, '1412811133', '1418513119');
 
 -- Inserir dados na tabela 'cabras' (AVÓS MATERNOS)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1650113018', 'JOSA CAPRIMEL', 'MALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ATIVO', 1, 1, '1403110395', '1650112018');
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('2114513061', 'PANTALONA DO CRS', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ATIVO', 1, 1, '2104406006', '2114510040');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1650113018', 'JOSA CAPRIMEL', 'MALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ACTIVE', 1, 1, '1403110395', '1650112018');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('2114513061', 'PANTALONA DO CRS', 'FEMALE', 'SAANEN', 'Indefinida', '2013-01-01', 'ACTIVE', 1, 1, '2104406006', '2114510040');
 
 -- Inserir dados na tabela 'cabras' (PAIS)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1635717065', 'C.V.C SIGNOS PETROLEO', 'MALE', 'SAANEN', 'Indefinida', '2017-01-01', 'ATIVO', 1, 1, '1422915618', '1422913470');
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('2114517012', 'NAIDE DO CRS', 'FEMALE', 'SAANEN', 'Indefinida', '2017-01-01', 'ATIVO', 1, 1, '1650113018', '2114513061');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1635717065', 'C.V.C SIGNOS PETROLEO', 'MALE', 'SAANEN', 'Indefinida', '2017-01-01', 'ACTIVE', 1, 1, '1422915618', '1422913470');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('2114517012', 'NAIDE DO CRS', 'FEMALE', 'SAANEN', 'Indefinida', '2017-01-01', 'ACTIVE', 1, 1, '1650113018', '2114513061');
 
 -- Inserir dados na tabela 'cabras' (FILHOS)
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id, tod, toe, categoria) VALUES ('1643218012', 'XEQUE V DO CAPRIL VILAR', 'MALE', 'SAANEN', 'CHAMOISÉE', '2018-06-27', 'ATIVO', 1, 1, '1635717065', '2114517012', '16432', '18012', 'PO');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro, tod, toe, categoria) VALUES ('1643218012', 'XEQUE V DO CAPRIL VILAR', 'MALE', 'SAANEN', 'CHAMOISÉE', '2018-06-27', 'ACTIVE', 1, 1, '1635717065', '2114517012', '16432', '18012', 'PO');
 
-INSERT INTO cabras (num_registro, nome, sexo, raca, cor, nascimento, status, capril_id, user_id, pai_id, mae_id) VALUES ('1643218013', 'IRMÃO DO XEQUE', 'MALE', 'SAANEN', 'CHAMOISÉE', '2020-01-01', 'ATIVO', 1, 1, '1635717065', '2114517012');
+INSERT INTO cabras (num_registro, nome, sexo, raca, cor, data_nascimento, status, capril_id, usuario_id, pai_num_registro, mae_num_registro) VALUES ('1643218013', 'IRMÃO DO XEQUE', 'MALE', 'SAANEN', 'CHAMOISÉE', '2020-01-01', 'ACTIVE', 1, 1, '1635717065', '2114517012');
 
-INSERT INTO eventos (goat_id, tipo_evento, data, descricao, local, veterinario, resultado) VALUES ('2114517012', 'PARTO', '2025-05-08', 'Nascimento de dois cabritos machos sem complicações.', 'Capril Vilar', 'Dr. João Silva', 'Ambos os cabritos nasceram saudáveis.');
+INSERT INTO eventos (goat_registration_number, tipo_evento, data, descricao, local, veterinario, resultado) VALUES ('2114517012', 'PARTO', '2025-05-08', 'Nascimento de dois cabritos machos sem complicações.', 'Capril Vilar', 'Dr. João Silva', 'Ambos os cabritos nasceram saudáveis.');
 
-INSERT INTO eventos (goat_id, tipo_evento, data, descricao, local, veterinario, resultado) VALUES ('2114517012', 'SAUDE', '2025-05-05', 'Apresentou sintomas de mastite clínica', 'Capril Vilar', 'Dr. Carlos Mendes', 'Iniciado tratamento com antibiótico específico. Isolamento recomendado por 5 dias.');
+INSERT INTO eventos (goat_registration_number, tipo_evento, data, descricao, local, veterinario, resultado) VALUES ('2114517012', 'SAUDE', '2025-05-05', 'Apresentou sintomas de mastite clínica', 'Capril Vilar', 'Dr. Carlos Mendes', 'Iniciado tratamento com antibiótico específico. Isolamento recomendado por 5 dias.');
