c1e01a6dcf9e04d44c5df778bc9e6374a65bb609
Alberto Vilar <albertovilar1@gmail.com>
2026-09-22 01:46:26 -0300

    fix: exclude reversed animal sales from financials

--- src/main/java/com/devmaster/goatfarm/commercial/persistence/repository/AnimalSaleReversalRepository.java
+++ src/main/java/com/devmaster/goatfarm/commercial/persistence/repository/AnimalSaleReversalRepository.java
@@ -3,8 +3,11 @@ package com.devmaster.goatfarm.commercial.persistence.repository;
 import com.devmaster.goatfarm.commercial.persistence.entity.AnimalSaleReversal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface AnimalSaleReversalRepository extends JpaRepository<AnimalSaleReversal, Long> {
     Optional<AnimalSaleReversal> findBySale_Id(Long saleId);
+    List<AnimalSaleReversal> findBySale_IdIn(Collection<Long> saleIds);
 }
